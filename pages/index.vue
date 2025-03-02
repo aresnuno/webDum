74847000aadf19d4e427815f34e77c35a573400c
Joao Soares <joaosoares@Mac-mini-Joao.local>
2025-03-03 02:12:44 +0700

    update vulnerablity issue and missing dependency

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
       <client-only>
-        <tiptap-editor />
       </client-only>
     </div>
   </template>
   <script>
-    import TiptapEditor from '~/components/TiptapEditor.vue'
     export default {
       components: {
-        TiptapEditor,
       },
     }
   </script>
